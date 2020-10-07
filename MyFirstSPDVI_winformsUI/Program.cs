99ae1b935d31a8b555ab5fb01aed7adddf92148a
Carlos Gomila <carlosgomila@paucasesnovescifp.cat>
2020-10-07 19:33:11 +0200

    created first vs2019 solution with a console and winforms project we have created a solution in visual studio and added two projects. the frst proect is a console app that shows a hello message. the second prject is a windows forms app that shows a hello message when we click a button

---
+++ MyFirstSPDVI_winformsUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstSPDVI_consuleUi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hellow spdvi 2021");
+            Console.ReadKey();
+        }
+    }
+}
